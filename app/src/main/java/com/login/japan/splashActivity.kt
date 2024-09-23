515ac90b3782efe3acb2d9cc76943456f864a252
Betrand-Alfayedh <btrnd.alfydh@gmail.com>
2024-09-24 01:21:33 +0700

    first commit

---
+++ app/src/main/java/com/login/japan/splashActivity.kt
@@ -0,0 +1,31 @@
+package com.login.japan
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.Button
+import com.nashwa.japan.R
+
+class splashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash2)
+
+        // Tombol "See Details" ketika diklik
+        val buttonDetails: Button = findViewById(R.id.buttonDetails)
+        buttonDetails.setOnClickListener {
+            // Pindah ke MainActivity
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
+
+        // Opsi untuk auto-pindah setelah beberapa detik (misalnya 3 detik)
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 1000) // Delay 3 detik
+    }
+}
